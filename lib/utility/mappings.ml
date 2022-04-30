29b3028cb074bb947bce16e08e2c19809af7c54d
Tetsuki Syu <deadshot465@gmail.com>
2022-04-30 17:37:37 +0900

    Update mappings.

--- lib/utility/mappings.ml
+++ lib/utility/mappings.ml
@@ -102,12 +102,118 @@ module Mappings = struct
     in
     Word.replace o_to_owo replacement word
 
-  let map_ew_to_uwu word = Word.replace ew_to_uwu "uwu" word
-  let map_hey_to_hay word = Word.replace hey_to_hay "\\1ay" word
+  let map_ew_to_uwu = Word.replace ew_to_uwu "uwu"
+  let map_hey_to_hay = Word.replace hey_to_hay "\\1ay"
   
   let map_dead_to_ded word =
     Word.replace dead_to_ded_upper "Ded" word
     |> Word.replace dead_to_ded_lower "ded"
 
-  let map_n_vowel_t_to_nd word = Word.replace n_vowel_t_to_nd "nd" word
+  let map_n_vowel_t_to_nd = Word.replace n_vowel_t_to_nd "nd"
+
+  let map_read_to_wead word =
+    Word.replace read_to_wead_upper "Wead" word |> Word.replace read_to_wead_lower "wead"
+
+  let map_brackets_to_star_trails word =
+    Word.replace brackets_to_startrails_fore "｡･:*:･ﾟ★,｡･:*:･ﾟ☆" word
+    |> Word.replace brackets_to_startrails_rear "☆ﾟ･:*:･｡,★ﾟ･:*:･｡"
+
+  let map_period_comma_exclamation_semicolon_to_kaomojis word =
+    let f = fun () ->
+      Array.length faces |> Random.int |> Array.get faces in
+    Word.replace_with_func_single period_comma_exclamation_semicolon_to_kaomojis_first f word
+    |> Word.replace_with_func_single period_comma_exclamation_semicolon_to_kaomojis_second f
+
+  let map_that_to_dat word =
+    Word.replace that_to_dat_lower "dat" word
+    |> Word.replace that_to_dat_upper "Dat"
+
+  let map_th_to_f word =
+    Word.replace th_to_f_lower "f" word
+    |> Word.replace th_to_f_upper "F"
+
+  let map_le_to_wal = Word.replace le_to_wal "wal"
+
+  let map_ve_to_we word =
+    Word.replace ve_to_we_lower "we" word
+    |> Word.replace ve_to_we_upper "We"
+
+  let map_ry_to_wwy = Word.replace ry_to_wwy "wwy"
+
+  let map_r_or_l_to_w word =
+    Word.replace rorl_to_w_lower "w" word
+    |> Word.replace rorl_to_w_upper "W"
+
+  let map_ll_to_ww = Word.replace ll_to_ww "ww"
+
+  let map_vowel_or_r_except_o_l_to_wl word =
+    Word.replace vowel_or_r_except_o_l_to_wl_lower "wl" word
+    |> Word.replace vowel_or_r_except_o_l_to_wl_upper "W\\1"
+
+  let map_old_to_owld word =
+    Word.replace old_to_owld_lower "\\1wld" word
+    |> Word.replace old_to_owld_upper "OWLD"
+
+  let map_ol_to_owl word =
+    Word.replace ol_to_owl_lower "\\1wl" word
+    |> Word.replace ol_to_owl_upper "OWL"
+
+  let map_l_or_r_o_to_wo word =
+    Word.replace lorr_o_to_wo_lower "wo" word
+    |> Word.replace lorr_o_to_wo_upper "W\\1"
+
+  let map_specific_consonants_o_to_letter_and_wo word =
+    let f = fun s1 s2 ->
+      if String.uppercase_ascii s2 = s2 then
+        Printf.sprintf "%s%s%s" s1 "W" s2
+      else
+        Printf.sprintf "%s%s%s" s1 "w" s2
+    in
+    Word.replace specific_consonants_o_to_letter_and_wo_lower "\\1wo" word
+    |> Word.replace_with_func_multiple specific_consonants_o_to_letter_and_wo_upper f
+
+  let map_v_or_w_le_to_wal = Word.replace vorw_le_to_wal "wal"
+
+  let map_fi_to_fwi word =
+    Word.replace fi_to_fwi_lower "\\1wi" word
+    |> Word.replace fi_to_fwi_upper "FWI"
+
+  let map_ver_to_wer = Word.replace ver_to_wer "wer"
+  let map_poi_to_pwoi = Word.replace poi_to_pwoi "\\1woi"
+  let map_specific_consonants_le_to_letter_and_wal = Word.replace specific_consonants_le_to_letter_and_wal "\\1wal"
+  let map_consonant_r_to_consonant_w = Word.replace consonant_r_to_consonant_w "\\1w"
+  
+  let map_ly_to_wy word =
+    Word.replace ly_to_wy_lower "wy" word
+    |> Word.replace ly_to_wy_upper "Wy"
+
+  let map_ple_to_pwe = Word.replace ple_to_pwe "\\1we"
+
+  let map_nr_to_nw word =
+    Word.replace nr_to_nw_lower "nw" word
+    |> Word.replace nr_to_nw_upper "NW"
+
+  let map_fuc_to_fwuc = Word.replace fuc_to_fwuc "\\1wuc"
+  let map_mom_to_mwom = Word.replace mom_to_mwom "\\1wom"
+  let map_me_to_mwe = Word.replace me_to_mwe "\\1we"
+
+  let map_n_vowel_to_ny word =
+    Word.replace n_vowel_to_ny_first "ny\\1" word
+    |> Word.replace n_vowel_to_ny_second "Ny\\1"
+    |> Word.replace n_vowel_to_ny_third "NY\\1"
+
+  let map_ove_to_uv word =
+    Word.replace ove_to_uv_lower "uv" word
+    |> Word.replace ove_to_uv_upper "UV"
+
+  let map_haha_to_hehe_xd = Word.replace haha_to_hehe_xd "hehe xD"
+  let map_the_to_teh = Word.replace the_to_teh "\\1eh"
+
+  let map_you_to_u word =
+    Word.replace you_to_u_upper "U" word
+    |> Word.replace you_to_u_lower "u"
+
+  let map_time_to_tim = Word.replace time_to_tim "\\1im"
+  let map_over_to_owor = Word.replace over_to_owor "\\1wor"
+  let map_worse_to_wose = Word.replace worse_to_wose "\\1ose"
 end
